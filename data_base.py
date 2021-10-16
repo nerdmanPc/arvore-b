c8166c9cfcced93c005655bc7b33bfca26c08613
lailamt <lakamota@gmail.com>
2021-10-16 15:34:53 -0300

    Teste

--- data_base.py
+++ data_base.py
@@ -1,130 +1,141 @@
 from typing_extensions import ParamSpecArgs
-from node import *
+from node import Node, Entry
 from main import GRAUMINIMO, FILE_PATH
 
-class OpStatus(Enum): 
-	OK = 0 
-	ERR_KEY_EXISTS = -1
-	ERR_OUT_OF_SPACE = -2
-	ERR_KEY_NOT_FOUND = -3
 
-#LAYOUT: | N | NO[0] | NÓ[1] | ... | NÓ[N-1]                   
+class OpStatus(Enum):
+    OK = 0
+    ERR_KEY_EXISTS = -1
+    ERR_OUT_OF_SPACE = -2
+    ERR_KEY_NOT_FOUND = -3
+
+
+# LAYOUT: | N | NÓ[0] | NÓ[1] | ... | NÓ[N-1]
 class DataBase:
-	header_format = Struct('>L')
+    header_format = Struct('>L')
 
-	# CERTO
-	def __init__(self, file_path:str):
-		self.path = file_path
-		try:
-			with open(file_path, 'xb') as file:
-				self.length = 0
-				file.write( self.header_format.pack(self.length) )
-				#empty_entry = Entry.from_status(EntryStatus.EMPTY).into_bytes()
-				#file.write(empty_entry * self.length)
-		except FileExistsError:
-			with open(file_path, "rb") as file:
-				header = file.read(self.header_size())
-				_tuple = self.header_format.unpack(header)
-				self.length = _tuple[0]
-	
-	# TODO: Inicializa o índice e coloca o ponteiro na raiz
-	def __iter__(self):
-		print('TODO: DataBase.__iter__()')
-		self.it_index = -1
-		self.it_file = open(self.path, 'rb')
-		self.it_file.seek(self.header_size(), 0)
-		return self
+    # CERTO
+    def __init__(self, file_path: str):
+        self.path = file_path
+        try:
+            with open(file_path, 'xb') as file:
+                self.length = 0
+                file.write(self.header_format.pack(self.length))
+        except FileExistsError:
+            with open(file_path, "rb") as file:
+                header = file.read(self.header_size())
+                _tuple = self.header_format.unpack(header)
+                self.length = _tuple[0]
 
-	# TODO: Itera sobre elementos da árvore
-	def __next__(self):
-		print('TODO: DataBase.__next__()')
-		self.it_index += 1
-		if self.it_index >= self.length:
-			self.it_file.close()
-			raise StopIteration
-		data = self.it_file.read(Entry.size())
-		entry = Entry.from_bytes(data)
-		return entry
-	
-	# TODO: IMPRIMIR A ÁRVORE
-	def __str__(self):
-		print('TODO: DataBase.__str__()')
-		result = []
-		for entry in self:
-			result.append( str(entry) )
-		return '\n'.join(result)
+    # TODO: Inicializa o índice e coloca o ponteiro na raiz
+    def __iter__(self):
+        print('TODO: DataBase.__iter__()')
+        self.it_index = -1
+        self.it_file = open(self.path, 'rb')
+        self.it_file.seek(self.header_size(), 0)
+        return self
 
-	# CERTO
-	@classmethod
-	def header_size(cls) -> int:
-		return cls.header_format.size
+    # TODO: Itera sobre elementos da árvore
+    def __next__(self):
+        print('TODO: DataBase.__next__()')
+        self.it_index += 1
+        if self.it_index >= self.length:
+            self.it_file.close()
+            raise StopIteration
+        data = self.it_file.read(Entry.size())
+        entry = Entry.from_bytes(data)
+        return entry
 
-	# CERTO - Note que antes, retornava um ponteiro pra registro, agora retorna um ponteiro pra nó
-	@classmethod
-	def index_to_ptr(cls, index:int) -> int:
-		node_size = Node.size()
-		header_size = cls.header_size()
-		return header_size + index * node_size
+    # TODO: IMPRIMIR A ÁRVORE
+    def __str__(self):
+        print('TODO: DataBase.__str__()')
+        result = []
+        for entry in self:
+            result.append(str(entry))
+        return '\n'.join(result)
 
-	# CERTO
-	def set_length(self, length:int) -> None:
-		with open(self.path, 'r+b') as file:
-			file.seek(0, 0)
-			self.length = length
-			file.write(self.header_format.pack(length))
+    # CERTO
+    @classmethod
+    def header_size(cls) -> int:
+        return cls.header_format.size
 
-	# TODO: Assim como outras funções, esta tem que mudar a semântica 
-	# para oprerar sobre NÓS em vez de REGISTROS
-	def node_by_index(self, index:int) -> Node:
-		print('TODO: DataBase.node_by_index()')
-		if index >= self.length: print(f'ÍNDICE INVÁLIDO: {index}')
-		with open(self.path, 'rb') as file:
-			file.seek(self.header_size() + index * Entry.size(), 0)
-			data = file.read(Entry.size())
-			entry = Entry.from_bytes(data)
-			return entry
+    # CERTO - Note que antes, retornava um ponteiro pra registro, agora retorna um ponteiro pra nó
+    @classmethod
+    def index_to_ptr(cls, index: int) -> int:
+        node_size = Node.size()
+        header_size = cls.header_size()
+        return header_size + index * node_size
 
-	# TODO: Escreve os bytes de um nó no final do arquivo e retona seu índice
-	# Responsável por alocar o nó.
-	def append_node(self, to_append: Node) -> int:
-		print('TODO: DataBase.append_node()')
+    # CERTO
+    def set_length(self, length: int) -> None:
+        with open(self.path, 'r+b') as file:
+            file.seek(0, 0)
+            self.length = length
+            file.write(self.header_format.pack(length))
 
-	# TODO; PERCORRER A ARVORE PRA ACHAR O NÓ DO REGISTRO
-	def add_entry(self, key:int, name:str, age:int) -> OpStatus:
-		print('TODO: DataBase.add_entry()')
-		with open(self.path, 'r+b') as file:
-			free_pointer = self.index_to_ptr(self.length)
-			file.seek(free_pointer)
-			entry_bytes = Entry(key, name, age).into_bytes()
-			file.write(Node.insert_in_leaf(entry_bytes))
-		return OpStatus.OK
+    # TODO: Assim como outras funções, esta tem que mudar a semântica
+    # para oprerar sobre NÓS em vez de REGISTROS
+    def node_by_index(self, index: int) -> Node:
+        print('TODO: DataBase.node_by_index()')
+        if index >= self.length: print(f'ÍNDICE INVÁLIDO: {index}')
+        with open(self.path, 'rb') as file:
+            file.seek(self.header_size() + index * Entry.size(), 0)
+            data = file.read(Entry.size())
+            entry = Entry.from_bytes(data)
+            return entry
 
-	# TODO: ACHA O REGISTRO DA CHAVE QUE PASSAR
-	def search_by_key(self, key:int) -> Optional[Entry]:
-		print('TODO: DataBase.search_by_key()')
-		(entry, key, aux) = Node.search_by_key(key)
-		if key is not None:
-			#busca
-			return
-		else:
-			return
+    # TODO: Escreve os bytes de um nó no final do arquivo e retona seu índice
+    # Responsável por alocar o nó.
+    def append_node(self, to_append: Node) -> int:
+        print('TODO: DataBase.append_node()')
 
-	# TODO: IMPRIME A ARVORE
-	# Os apontadores e chaves devem ser impressos seguindo a estrutura do nó. Cada apontador deve ser impresso da seguinte maneira: a sequência de caracteres ’apontador:’, seguida de um espaço, seguido do número sequencial do nó para o qual o apontador aponta. Se for um nó folha, o valor dos apontadores deve ser null. Cada chave será impressa da seguinte maneira: a sequência de caracteres ’chave:’, seguida de um espaço, seguido do valor da chave. As impressões de apontadores e chaves devem estar separadas por um espaço. Se em um nó estiverem armazenados k registros, apenas as chaves destes registros e os apontares adjacentes a eles devem ser impressos.
-	def print_tree(self, node:int, depth:int):
-		print('TODO: DataBase.print_tree()')
-		depth = 0
-		print('No: ', no, ': ')
-		for child in node:
-			print(apontar/chave, ': ', valor)
-		print('\n')
-		depth += 1
-		pass
+    # TODO; PERCORRER A ARVORE PRA ACHAR O NÓ DO REGISTRO
+    def add_entry(self, key: int, name: str, age: int) -> OpStatus:
+        print('TODO: DataBase.add_entry()')
+        with open(self.path, 'r+b') as file:
+            free_pointer = self.index_to_ptr(self.length)
+            file.seek(free_pointer)
+            entry_bytes = Entry(key, name, age).into_bytes()
+            file.write(Node.insert_in_leaf(entry_bytes))
+        return OpStatus.OK
 
-	# TODO: IMPRIME A ÁRVORE ORDENADA
-	def print_keys_ordered(self):
-		print('TODO: DataBase.print_keys_ordered()')
-	
-	# TODO: IMPRIME A TAXA DE OCUPAÇÃO
-	def print_occupancy(self):
-		print('TODO: DataBase.print_occupancy()')
+    # TODO: ACHA O REGISTRO DA CHAVE QUE PASSAR
+    def search_by_key(self, key: int) -> Optional[Entry]:
+        print('TODO: DataBase.search_by_key()')
+        (entry, key, aux) = Node.search_by_key(key)
+        if key is not None:
+            # busca
+            return
+        else:
+            return
+
+    # TODO: IMPRIME A ARVORE
+    # Os apontadores e chaves devem ser impressos seguindo a estrutura do nó.
+    # Cada apontador deve ser impresso da seguinte maneira:
+    # a sequência de caracteres ’apontador:’, seguida de um espaço,
+    # seguido do número sequencial do nó para o qual o apontador aponta.
+    # Se for um nó folha, o valor dos apontadores deve ser null.
+    # Cada chave será impressa da seguinte maneira:
+    # a sequência de caracteres ’chave:’, seguida de um espaço,
+    # seguido do valor da chave. As impressões de apontadores
+    # e chaves devem estar separadas por um espaço.
+    # Se em um nó estiverem armazenados k registros,
+    # apenas as chaves destes registros e os apontares adjacentes a
+    # eles devem ser impressos.
+    def print_tree(self, node: int, depth: int):
+        print('TODO: DataBase.print_tree()')
+        depth = 0
+        print('No: ', no, ': ')
+        for child in node:
+            print(apontar / chave, ': ', valor)
+        print('\n')
+        depth += 1
+        pass
+
+    # TODO: IMPRIME A ÁRVORE ORDENADA
+    def print_keys_ordered(self):
+        print('TODO: DataBase.print_keys_ordered()')
+
+    # TODO: IMPRIME A TAXA DE OCUPAÇÃO
+    def print_occupancy(self):
+        print('TODO: DataBase.print_occupancy()')
--- node.py
+++ node.py
@@ -1,14 +1,16 @@
 from struct import Struct
 from typing import Optional, Tuple, Union, List
 from enum import Enum
-#from main import GRAUMINIMO
+
+# from main import GRAUMINIMO
 
 GRAUMINIMO = 2
+
 
 class Entry:
     format = Struct('> L 20s B')
 
-    def __init__(self, key:int, name:str, age:int) -> None:
+    def __init__(self, key: int, name: str, age: int) -> None:
         self._key = key
         self._name = name
         self._age = age
@@ -29,8 +31,8 @@ class Entry:
     def __str__(self):
         return '{} {} {}'.format(self._key, self._name, self._age)
 
-    @classmethod 
-    def from_bytes(cls, data: bytes): #-> Entry
+    @classmethod
+    def from_bytes(cls, data: bytes):  # -> Entry
         (key, name, age) = cls.format.unpack(data)
         name = str(name, 'utf-8')
         return Entry(key, name, age)
@@ -38,10 +40,10 @@ class Entry:
     def into_bytes(self) -> bytes:
         return self.format.pack(self._key, bytes(self._name, 'utf-8'), self._age)
 
-class Node:
 
-    max_degree = 2*GRAUMINIMO
-    header_format = Struct('> ? I') #is_leaf: bool, entry_count: uint32
+class Node:
+    max_degree = 2 * GRAUMINIMO
+    header_format = Struct('> ? I')  # is_leaf: bool, entry_count: uint32
     child_id_format = Struct('> I')
     entry_format = Entry.format
     header_size = header_format.size
@@ -61,25 +63,25 @@ class Node:
     def new_root(cls, entry: Entry, left, right):
         return cls(False, [entry], [left, right])
 
-    # Deve ser chamada logo após 'insert_in_parent()', 
-    # no nó retornado por esta. 
+    # Deve ser chamada logo após 'insert_in_parent()',
+    # no nó retornado por esta.
     # sendo 'key' a chave recém inserida. Retorna o novo nó.
-    def split_by_key(self, key: int): #-> Node 
+    def split_by_key(self, key: int):  # -> Node
         print('TODO: Node.slpit_by_key()')
 
     # Deve ser chamada ao visitar o nó e ele estar cheio.
     # Retorna a tupla (chave_removida, novo_no)
-    def split_when_full(self): #-> Tuple[Entry, Node]:
+    def split_when_full(self):  # -> Tuple[Entry, Node]:
         print('TODO: node.split_when_full')
-    
+
     @classmethod
-    def from_bytes(cls, data: bytes): #-> Node:
+    def from_bytes(cls, data: bytes):  # -> Node:
         (is_leaf, entry_count) = cls.header_format.unpack(data[:cls.header_size])
 
         entries = []
         for index in range(entry_count):
             ptr = cls._entry_offset(index)
-            entry_data = data[ ptr : ptr + cls.entry_size ]
+            entry_data = data[ptr: ptr + cls.entry_size]
             entry = Entry.from_bytes(entry_data)
             entries.append(entry)
 
@@ -89,7 +91,7 @@ class Node:
         children = []
         for index in range(entry_count + 1):
             ptr = cls._child_offset(index)
-            child_data = data[ ptr : ptr + cls.child_id_size ]
+            child_data = data[ptr: ptr + cls.child_id_size]
             child = cls.child_id_format.unpack(child_data)
             children.append(child)
 
@@ -102,7 +104,7 @@ class Node:
         for index, entry in enumerate(self._entries):
             ptr = self._entry_offset(index)
             entry_data = entry.into_bytes()
-            data[ ptr : ptr + self.entry_size ] = entry_data
+            data[ptr: ptr + self.entry_size] = entry_data
 
         if self._is_leaf:
             return bytes(data)
@@ -110,81 +112,91 @@ class Node:
         for index, child in enumerate(self._children_ids):
             ptr = self._child_offset(index)
             child_data = self.child_id_format.pack(child)
-            data[ ptr : ptr + self.child_id_size ] = child_data
-
+            data[ptr: ptr + self.child_id_size] = child_data
         return bytes(data)
 
-    #Insere registro 'to_insert', aloca espaço para um novo ID de filho 
-    # e retorna o ID do filho a ser dividido. 
-    def insert_in_parent(self, to_insert: Entry) -> Optional[int]: 
-        #  |Registro|Ponteiro|Registro| -> |Registro|Ponteiro|NovoRegistro|NovoPonteiro|Registro|
-        #  |index-1 |     index       | -> |index-1 |        index        |      index+1        |
+    # Insere registro 'to_insert', aloca espaço para um novo ID de filho
+    # e retorna o ID do filho a ser dividido.
+    #  |Registro|Ponteiro|Registro| -> |Registro|Ponteiro|NovoRegistro|NovoPonteiro|Registro|
+    #  |index-1 |     index       | -> |index-1 |        index        |      index+1        |
+    def insert_in_parent(self, to_insert: Entry) -> Optional[int]:
         if self._is_leaf() or self.is_full():
             return None
         for index, current in enumerate(self._entries):
             if current.key_greater_than(to_insert.key()):
-                self.entriesert_in_leaf(entry_b)
-    #Insere registr 'to_insert' e retorna se houve sucesso
+                self._entries.insert(index, to_insert)  # insere 'to_insert' antes de 'current'
+                self._children_ids.insert(index + 1,
+                                          -1)  # insere índice inválido (-1) no espaço que receberá o próximo nó
+                return self._children_ids[index]  # retorna anterior ao inválido, que será dividido
+        self._entries.append(to_insert)  # Se nenhum registro tem a chave maior, insere no final
+        self._children_ids.append(-1)  # insere índice inválido (-1) no espaço que receberá o próximo nó (último filho)
+        return self._children_ids[-2]  # retorna anterior ao inválido, que será dividido
+
+    # Insere registr 'to_insert' e retorna se houve sucesso
     def insert_in_leaf(self, to_insert: Entry) -> bool:
         if (not self._is_leaf) or self.is_full():
             return False
         for index, current in enumerate(self._entries):
             if current.key_greater_than(to_insert.key()):
-                self._entries.insert(index, to_insert) #insere 'to_insert' antes de 'current'
+                self._entries.insert(index, to_insert)  # insere 'to_insert' antes de 'current'
                 return True
-        self._entries.append(to_insert) # Se nenhum registro tem a chave mair, insere no final
+        self._entries.append(to_insert)  # Se nenhum registro tem a chave maior, insere no final
         return True
 
     # Insere 'child_id' no lugar do primeiro ponteiro inválida (-1).
     # Deve ser chamada depois de insert_in_parent() com o ID  do novo nó.
     def insert_child(self, child_id: int) -> None:
-        print('TODO: node.insert_child()')
-            
-    #Retorna Entry se a chave está no nó, int se está num nó filho e None se 
+        for child in self._children_ids:
+            if child == -1:
+                child = child_id
+                return
+
+    # Retorna Entry se a chave está no nó, int se está num nó filho e None se
     # chave não está no nó e este nó é folha. Busca apenas dentro do próprio nó.
-    def search_by_key(self, key: int) -> Union[Entry, int, None]: 
+    def search_by_key(self, key: int) -> Union[Entry, int, None]:
         print('TODO: Node.search_by_key()')
 
     def is_full(self) -> bool:
-        return len(self._entries) >= self.max_degree-1
+        return len(self._entries) >= self.max_degree - 1
 
     def is_leaf(self) -> bool:
         return self._is_leaf
-        
+
     @classmethod
     def size(cls) -> int:
         return cls.header_size + \
-            cls.child_id_size * cls.max_degree + \
-            cls.entry_size * (cls.max_degree - 1)
+               cls.child_id_size * cls.max_degree + \
+               cls.entry_size * (cls.max_degree - 1)
 
     @classmethod
-    def _entry_offset(cls, index: int) -> Optional[Entry]: #PRIVADO
-        start = cls.header_size + cls.child_id_size #Pula o cabeçalho e o primeiro filho
-        step = cls.entry_size + cls.child_id_size #Avança para o próximo registro pulando o filho entre eles
-        return start + index*step
+    def _entry_offset(cls, index: int) -> Optional[Entry]:  # PRIVADO
+        start = cls.header_size + cls.child_id_size  # Pula o cabeçalho e o primeiro filho
+        step = cls.entry_size + cls.child_id_size  # Avança para o próximo registro pulando o filho entre eles
+        return start + index * step
 
     @classmethod
-    def _child_offset(cls, index: int) -> Optional[int]: #PRIVADO
-        start = cls.header_size #Pula o cabeçalho
-        step = cls.child_id_size + cls.entry_size #Avança para o próximo filho pulando o registro entre eles
-        return start + step * index 
+    def _child_offset(cls, index: int) -> Optional[int]:  # PRIVADO
+        start = cls.header_size  # Pula o cabeçalho
+        step = cls.child_id_size + cls.entry_size  # Avança para o próximo filho pulando o registro entre eles
+        return start + step * index
 
     def __iter__(self):
         return iter(self._entries)
 
-    def __str__(self) -> str: 
+    def __str__(self) -> str:
         items_str = []
         for index, entry in enumerate(self._entries):
             if index < len(self._children_ids):
-                child = self._children_ids[index] 
+                child = self._children_ids[index]
                 items_str.append(str(child))
             items_str.append(str(entry))
         if not self._is_leaf:
-            child = self._children_ids[-1] 
+            child = self._children_ids[-1]
             items_str.append(str(child))
         return ' | '.join(items_str)
 
-#TESTE
+
+# TESTE
 
 entries = [
     Entry(0, 'Roberto Carlos', 255),
@@ -198,13 +210,15 @@ nodes = [Node.new_empty()]
 root_index = 0
 root: Entry = nodes[root_index]
 
+
 def append_node(nodes, node: Node) -> int:
     new_index = len(nodes)
     nodes.append(node)
-    return new_index 
+    return new_index
+
 
 for entry in entries:
-    parent, current
+    # parent, current
     if root.is_full():
         (extracted_entry, new_node) = root.split_when_full()
         new_index = append_node(nodes, new_node)
@@ -219,14 +233,13 @@ for entry in entries:
         new_index = len(nodes)
         nodes.append(new_node)
         root.insert_child(new_index)
-    for i, node in enumerate(nodes): 
+    for i, node in enumerate(nodes):
         print(f'Node[{i}]: {node}')
 
-
-#print(nodes[0])
-#nodes[0].insert_in_leaf(entries[0])
-#print(nodes[0])
-#nodes[0].insert_in_leaf(entries[1])
-#print(nodes[0])
-#nodes[0].insert_in_leaf(entries[2])
-#print(nodes[0])
+# print(nodes[0])
+# nodes[0].insert_in_leaf(entries[0])
+# print(nodes[0])
+# nodes[0].insert_in_leaf(entries[1])
+# print(nodes[0])
+# nodes[0].insert_in_leaf(entries[2])
+# print(nodes[0])
